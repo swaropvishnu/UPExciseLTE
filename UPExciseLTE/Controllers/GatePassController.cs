13811ffecd73704d6a7c019f757061cd51fb8a99
swaropvishnu <swaropvishnu@gmail.com>
2018-10-25 16:04:26 +0530

    Vijay 25102018 0404PM

---
+++ UPExciseLTE/Controllers/GatePassController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UPExciseLTE.Models;
+
+namespace UPExciseLTE.Controllers
+{
+    public class GatePassController : Controller
+    {
+        // GET: GatePass
+
+        #region BreweryToManufacturerWholesale
+        public ActionResult BreweryToManufacturerWholesale()
+        {
+            List<SelectListItem> LicenceNos = new List<SelectListItem>();
+            ViewBag.LicenceNo = LicenceNos;
+            return View();
+        }
+        #endregion
+
+
+        #region ManufacturerWholesaleToDistrictWholesale
+        public ActionResult ManufacturerWholesaleToDistrictWholesale()
+        {
+            List<SelectListItem> LicenceNos = new List<SelectListItem>();
+            ViewBag.LicenceNo = LicenceNos;
+            return View();
+        }
+        #endregion
+
+
+        #region DistrictWholesaleToRetailor
+        public ActionResult DistrictWholesaleToRetailor()
+        {
+            List<SelectListItem> LicenceNos = new List<SelectListItem>();
+            BreweryToManufacturerWholesaleModel breweryToManufacturerWholesaleModel = new BreweryToManufacturerWholesaleModel();
+            breweryToManufacturerWholesaleModel.DistrictWholeSaleToRetailorList = new List<DistrictWholeSaleToRetailorModel>();
+            ViewBag.LicenceNo = LicenceNos;
+            return View(breweryToManufacturerWholesaleModel);
+        }
+        #endregion
+
+
+   
+
+
+
+
+    }
+}
---
+++ UPExciseLTE/Models/BreweryToManufacturerWholesaleModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UPExciseLTE.Models
+{
+    public class BreweryToManufacturerWholesaleModel
+    {
+
+        [DataType(DataType.Date)]
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+
+        [DataType(DataType.Date)]
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ValidTill { get; set; }
+
+        public string FromRdoList { get; set; }
+
+        public string SelectedFromRdo { get; set; }
+
+        public string ToRdoList { get; set; }
+
+        public string SelectedToRdo { get; set; }
+
+        public string    RouteDetails { get; set; }
+
+        public string VehicleNo { get; set; }
+
+        public string VehicleDriverName { get; set; }
+
+        public string VehicleAgencyNameAndAddress { get; set; }
+
+
+        public string  LicenceNo { get; set; }
+
+        public string LicenceName { get; set; }
+
+
+        public string Address { get; set; }
+
+        public string MajorDistrictsInRoute { get; set; }
+
+
+        public decimal GrossWeight { get; set; }
+
+        public decimal TareWeight { get; set; }
+
+
+        public decimal NetWeight { get; set; }
+
+
+        public IEnumerable<DistrictWholeSaleToRetailorModel> DistrictWholeSaleToRetailorList { get; set; }
+
+    }
+
+
+
+    public class DistrictWholeSaleToRetailorModel
+    {
+        [Display(Name ="S.No")]
+        public int SerialNo { get; set; }
+
+        [Display(Name = "Kind Of Liquor")]
+        public DateTime KindOfLiquor { get; set; }
+
+        [Display(Name ="Batch No.")]
+        public string BatchNo { get; set; }
+
+        [Display(Name ="Bottles,Flask Quantity Contained in each")]
+        public decimal Quantity { get; set; }
+
+        [Display(Name ="BL")]
+        public decimal TotalLitresOfBL { get; set; }
+        [Display(Name = "AL")]
+        public decimal TotalLitresOfAL { get; set; }
+
+        [Display(Name ="Alcoholic Strength")]
+        public string AlcohalicStrength { get; set; }
+
+       
+    }
+
+}
