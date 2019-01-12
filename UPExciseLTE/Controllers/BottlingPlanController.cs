3762e0c6e01290bd9162528e1e2bcdb2791474be
Finance <swaropvishnu@gmail.com>
2019-01-12 17:23:04 +0530

    Gaurav0522

---
+++ UPExciseLTE/Controllers/BottlingPlanController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UPExciseLTE.BLL;
+using UPExciseLTE.DAL;
+using UPExciseLTE.Models;
+
+namespace UPExciseLTE.Controllers
+{
+    public class BottlingPlanController : Controller
+    {
+        [HttpGet]
+        public ActionResult BottelingPlan()
+        {
+            BottelingPlan BP = new BottelingPlan();
+            ViewBag.Msg = TempData["Message"];
+            ViewBag.Brand = CommonBL.fillBrand("S");
+            ViewBag.Msg = TempData["Message"];
+            if (Request.QueryString["A"] != null && Request.QueryString["A"].ToString().Trim() != string.Empty)
+            {
+                BP = (new CommonBL().GetBottelingPlan(CommonBL.Setdate("01/01/1900"), DateTime.Now, short.Parse(CommonBL.fillBrewery()[0].Value), -1, "", "", int.Parse(new Crypto().Decrypt(Request.QueryString["A"].Trim())), "PB"));
+            }
+            return View(BP);
+        }
+        [HttpPost]
+        public ActionResult BottelingPlan(BottelingPlan BP)
+        {
+            try
+            {
+                BP.DateOfPlan = CommonBL.Setdate(BP.DateOfPlan1);
+            }
+            catch
+            {
+                ViewBag.Msg = "Invalid Date. Please use dd/MM/yyyy format.";
+                return View(BP);
+            }
+
+            string str = new CommonDA().InsertUpdatePlanBWFL(BP);
+            TempData["Message"] = str;
+            if (BP.Type == 1)
+            {
+                return RedirectToAction("BottelingPlan");
+            }
+            else
+            {
+                return RedirectToAction("BottelingPlan", new { A = BP.EncPlanId });
+            }
+        }
+    }
+}
